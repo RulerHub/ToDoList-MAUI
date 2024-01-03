1b2ba69c7f7e0b0d1a908b4f74e1f31f1225e0f4
Adrian Mesa <adrianmesasacasas@outlook.com>
2024-01-02 22:30:23 -0500

    Agregar archivos de proyecto.

---
+++ ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models;
+
+internal class ToDoItem
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public bool IsDone { get; set; } = false;
+}
