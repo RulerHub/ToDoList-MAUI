891fafc9de334429a35f6f44fc1308e746b89741
Adrian Mesa <adrianmesasacasas@outlook.com>
2024-01-04 20:42:14 -0500

    ReeHaciendo proyecto

---
+++ Projects/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Projects
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ToDoProject/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
